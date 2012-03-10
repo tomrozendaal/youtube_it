4e58f8c6a3789c07874193b7c121cecd58656ffa
Tom Rozendaal <tomrozendaal5434@gmail.com>
2012-03-10 18:00:05 +0100

    test

--- lib/youtube_it/parser.rb
+++ lib/youtube_it/parser.rb
@@ -579,9 +579,9 @@ class YouTubeIt
         #end          
         
         ytid = nil
-        unless media_group["yt:videoid"].nil?
+        #unless media_group["yt:videoid"].nil?
           ytid = media_group["yt:videoid"].text
-        end   
+        #end   
         
         YouTubeIt::Model::Video.new(
           :unique_id      => ytid)
