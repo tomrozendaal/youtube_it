24b2a749efbbb78d565595b4eb1721fa28c59a83
Tom Rozendaal <tomrozendaal5434@gmail.com>
2012-03-11 12:39:12 +0100

    test

--- lib/youtube_it/parser.rb
+++ lib/youtube_it/parser.rb
@@ -574,7 +574,7 @@ class YouTubeIt
           :videos             => videos)
       end
       def parse_history(entry)  
-        media_group = REXML::Document.new(entry.elements["media:group"])
+        media_group = REXML::Document.new(entry.elements["media:group"].text)
         
         ytid = media_group.elements["yt:videoid"].text
         
